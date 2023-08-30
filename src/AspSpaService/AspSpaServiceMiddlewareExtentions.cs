1dd02851c6dfb8f3699e3f67d9b30db57e65a543
kolesnikovav <a9104656059@yandex.ru>
2023-08-30 14:50:42 +0300

    Correct nodejs process disposing On branch windows Your branch is up to date with 'origin/windows'.
    Changes to be committed:
    	modified:   samples/webapi/Startup.cs
    	modified:   src/AspSpaService/AspSpaServiceMiddlewareExtentions.cs

--- src/AspSpaService/AspSpaServiceMiddlewareExtentions.cs
+++ src/AspSpaService/AspSpaServiceMiddlewareExtentions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.Hosting;
 
 namespace AspSpaService;
 
@@ -86,6 +87,95 @@ public static class AspSpaServiceMiddlewareExtensions
 
         }
     }
+    /// <summary>
+    /// Handles requests by passing them through to an instance of the node dev server.
+    /// This means you don't need to start node dev server manually.
+    ///
+    /// </summary>
+    /// <param name="spaBuilder">The <see cref="ApplicationBuilder"/>.</param>
+    /// <param name="applicationLifetime">The <see cref="IHostApplicationLifetime"/>.</param>
+    /// <param name="command">The command or file name to start dev server.</param>
+    /// <param name="arguments">Arguments to start dev server.</param>
+    /// <param name="workingDirectory">WorkingDirectory for node dev  server</param>
+    /// <param name="envVars">Environment variables for node dev  server</param>
+    /// <param name="timeout">Timeout for node process waiting</param>
+    /// <param name="timeoutExceedMessage">Message when timeout is exceeded</param>
+    /// <param name="logInformation">Log node process output</param>
+    /// <param name="logError">Log node js process error</param>
+    /// <param name="unsubscribeWhenReady">Stop logging when nodejs process is ready</param>
+    public static void UseAspSpaDevelopmentServer(
+        this ISpaBuilder spaBuilder,
+        IHostApplicationLifetime applicationLifetime,
+        string command,
+        string arguments,
+        string workingDirectory,
+        Dictionary<string, string> envVars,
+        TimeSpan timeout,
+        string timeoutExceedMessage = "Timeout has been exceeded ",
+        bool logInformation = true,
+        bool logError = false,
+        bool unsubscribeWhenReady = true)
+    {
+        if (spaBuilder == null)
+        {
+            throw new ArgumentNullException(nameof(spaBuilder));
+        }
+        var logger = GetOrCreateLogger(spaBuilder.ApplicationBuilder, LogCategoryName);
+        NodeRunner runner = GetNodeRunner(spaBuilder.ApplicationBuilder);
+        runner ??= new NodeRunner();
+        runner.Command = command;
+        if (OperatingSystem.IsWindows())
+        {
+            runner.Command = $"{command}.cmd";
+        }
+        runner.Arguments = arguments;
+        runner.WorkingDirectory = workingDirectory;
+        runner.EnvVars = envVars;
+        runner.Timeout = timeout;
+        runner.TimeoutExceedMessage = timeoutExceedMessage;
+        runner.Launch(logger);
+        if (runner.Uri != null)
+        {
+            if (unsubscribeWhenReady)
+            {
+                runner.UnsubscribeLog(logger);
+            }
+            spaBuilder.UseProxyToSpaDevelopmentServer(runner.Uri);
+        }
+        else
+        {
+
+        }
+        applicationLifetime.ApplicationStopping.Register(()=>{
+            NodeRunner r = GetNodeRunner(spaBuilder.ApplicationBuilder);
+            if (r != null) {
+                try
+                {
+                r.Dispose();
+                logger.LogInformation("Node JS Process disposed");                     
+
+                }
+                catch
+                {
+                    
+                }
+               
+            }
+        });        
+    }    
+
+
+    // public static void AddNodeRunner(this IServiceCollection services, IHostApplicationLifetime applicationLifetime)
+    // {
+    //     services.AddSingleton<NodeRunner>();
+    //     applicationLifetime.ApplicationStopping.Register(()=>{
+    //         Console.WriteLine("fsdasfasdfd");
+    //     });
+    // }
+
+
+
+
     private static ILogger GetOrCreateLogger(
         IApplicationBuilder appBuilder,
         string logCategoryName)
